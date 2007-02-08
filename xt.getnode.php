e724d9ea2a587b271938a8bcdc1f58d24df8138f
xoran <xoran@c929b04a-e628-0410-9d3c-ee9cb5ba2308>
2007-02-08 13:12:46 +0000

    better css3 support

--- xt.getnode.php
+++ xt.getnode.php
@@ -24,9 +24,7 @@
 class getNode{
 	public function __construct($xt){
 		$this->debug=1;
-		
-		$this->xpath='.';
-		
+
 		$this->xml=$xt->xml;
 		$this->root=$xt->root;
 		
@@ -34,7 +32,10 @@ class getNode{
 	}
 	
 	public function get($str, $parent=0){
+		$this->parent=$parent?$parent:$this->root;
 		if($this->method===2){
+			$this->xpath='.';
+		
 			$match=preg_split('#(\s*(?:>|(?<!n)\+|~(?!=))\s*|\s+)#', trim($str), -1, PREG_SPLIT_DELIM_CAPTURE);
 			array_unshift($match, null);
 			
@@ -44,10 +45,7 @@ class getNode{
 				echo '</pre>';
 			}
 			
-			$this->parent=$parent?$parent:$this->root;
-			
 			$count=count($match);
-			
 			for($i=0; $i<$count; $i+=2){
 				$str=$match[$i+1];
 				$glue=$match[$i];
@@ -66,17 +64,14 @@ class getNode{
 			
 			return $objects;
 		}elseif($this->method===1){
-			if(!$parent){
-				$parent=$this->root;
-			}
-			$xpath = new DOMXPath($this->xml);
-			$results = $xpath->query($this->xpath, $parent);
-			return $results;
+			$this->xpath=$str;
+			$objects=$this->getobjects();
+			return $objects;
 		}
 	}
 	
 	private function add($str, $glue=0){
-		$unicode='[0-9a-f]{1,6}(?:\r\n|[ \n\r\t\f])?';
+		/*$unicode='[0-9a-f]{1,6}(?:\r\n|[ \n\r\t\f])?';
 		$nonascii='[^\0-\177]';
 		$escape=$unicode.'|[^\n\r\f0-9a-f]';
 		
@@ -113,197 +108,269 @@ class getNode{
 		
 		preg_match_all('#(('.$type_selector.'|'.$universal.')('.$hash.'|'.$class.'|'.$attrib.'|'.$pseudo.'|'.$negation.')*|('.$hash.'|'.$class.'|'.$attrib.'|'.$pseudo.'|'.$negation.')+)#', $str, $match);
 		
-		print_r($match);
+		print_r($match);*/
 		
-
-		if(preg_match('#(.*?)((:first-child|:first-of-type|:last-child|:last-of-type|:nth-child\('.$r_nth.'\)|:nth-of-type\('.$r_nth.'\)|:root|:empty))#', $str, $match)){
-			$str=$match[1];
-			$pseudo_class=$match[2];
-		}
+		$name='[a-z0-9]+';
 		
 		$glue=$this->getglue($glue);
 		
-		if(preg_match('#^('.$r_name.'|\*)?\#('.$r_id.')$#', $str, $match)){
-			if($this->debug){echo '<p>po id <code>'.$str.'</code></p>';}
+		if(preg_match('#^'.$name.'#', $str, $match)){
+			// tak, mamy nazwę
+			$name=$match[0];
 			
-			$name=empty($match[1])?'*':$match[1];
-			$id=$match[2];
+			$this->xpath.=$glue.$name;
 			
-			$xpath=$name.'[@id="'.$id.'"]';
-		}elseif(preg_match('#^('.$r_name.'|\*)?((?:\.'.$r_id.')+)$#', $str, $match)){
-			if($this->debug){echo '<p>po klasie <code>'.$str.'</code></p>';}
+			$str=substr($str, strlen($name));
 			
-			$name=empty($match[1])?'*':$match[1];
-			$class=explode('.',substr($match[2], 1));
-			
-			$count=empty($match[3])?null:$match[3];
-			
-			$xpath=$name.'[';
-	
-			foreach($class as $c){
-				$xpath.='contains(concat(" ", @class, " "), " '.$c.' ") and ';
-			}
-			
-			$xpath=substr($xpath, 0, -4);
-			
-			$xpath.=']';
-		}elseif(preg_match('#^('.$r_name.'|\*)$#', $str, $match)){
-			if($this->debug){echo '<p>po nazwie <code>'.$str.'</code></p>';}
-			
-			$name=$match[1];
-			$count=$match[2];
-			
-			$xpath=$name;
-		/*
-			może być więcej nawiasów kwadratowych
-		*/
-		}elseif(preg_match('#('.$r_name.'|\*)?\[('.$r_attribute.')(?:([~^$*|]?)="('.$r_value.')"\])?#', $str, $match)){
-			if($this->debug){echo '<p>po atrybucie rozszerzonym <code>'.$str.'</code></p>';}
-			
-			
-			$name=empty($match[1])?'*':$match[1];
-			$attribute=$match[2];
-			$separator=$match[3];
-			$value=$match[4];
-			$count=empty($match[5])?null:$match[5];
-			
-			if($value){
-				switch($separator){
-					case '':
-						$match='[@'.$attribute.'="'.$value.'"]';
-						break;
-					case '~':
-						$match='[contains(concat(" ", @'.$attribute.', " "), " '.$value.' ")]';
-						break;
-					case '^':
-						$match='[starts-with(@'.$attribute.', '.$value.')]';
-						break;
-					case '$':
-						$match='[substring(@'.$attribute.', string-length(@'.$attribute.')-'.strlen($value).')="'.$value.'"]';
-						break;
-					case '*':
-						$match='[contains(@'.$attribute.', "'.$value.'")]';
-						break;
-					case '|':
-						$match='[contains(@'.$attribute.', " '.$value.'-") or starts-with(@'.$attribute.', "'.$value.'-")]';
-						break;
-				}
-			}else{
-				$match='[@'.$attribute.']';
-			}
-			
-			$xpath=$name.$match;
 		}else{
-			//exit('błąd parsowania ciągu <code>'.$str.'</code>');
+			// nie, nie podano nazwy
+			
+			$this->xpath.=$glue.'*';
 		}
-		/*
-			:first-child	[../*[position()=1]=.]
-			:last-child	[../*[position()=last()]=.]
-			:nth-child
-			:only-child	[../*[position()=1 and position()=last()]=.]
-			
-			:first-of-type	[position()=1]
-			:last-of-type	[position()=last()]
-			:nth-of-type
-			:only-of-type	[position()=1 and position()=last()]
-			
-			:root		[/=.]
-			:empty		[count(./child::node())=0]
-			
-			:lang(x)	to samo co [lang|="x"]
-		*/
 		
+		echo $str;
 		
-		if($pseudo_class){
-			if($pseudo_class==':first-child'){
-				$xpath='*/../*[1]/self::'.$xpath;
-			}
-			
-			if($pseudo_class==':last-child'){
-				$xpath='*/../*[last()]/self::'.$xpath;
-			}
-			
-			if($pseudo_class==':first-of-type'){
-				$xpath.='[1]';
-			}
-			
-			if($pseudo_class==':last-of-type'){
-				$xpath.='[last()]';
-			}
-			
-			if(substr($pseudo_class, 0, 10)==':nth-child'){
-			
-				if(preg_match('#(-?(\d+)?)n#', substr($pseudo_class, 11, -1), $a)){
-					if(empty($a[1])){
-						$a=1;
-					}elseif(empty($a[2])){
-						$a=-1;
-					}else{
-						$a=(int)$a[1];
-					}
-				}else{
-					$a=null;
-				}
-				
-				preg_match('#(-?\d+)(?!n)#', $pseudo_class, $b);
-				if(empty($b[1])){
-					$b=0;
-				}else{
-					$b=(int)$b[1];
-				}
-				
-				if($a==1){
-					$xpath='*/../*[position()>'.($b-1).']/self::'.$xpath;
-				}elseif($a==-1){
-					$xpath='*/../*[position()<'.($b-1).']/self::'.$xpath;
-				}elseif($a){
-					$xpath='*/../*[(position()+'.(-$b).')*'.$a.'>=0 and (position()+'.(-$b).') mod '.$a.'=0]/self::'.$xpath;
-				}else{
-					$xpath='*/../*[position()='.$b.']/self::'.$xpath;
-				}
-			}
-			
-			if(substr($pseudo_class, 0, 12)==':nth-of-type'){
-			
-				if(preg_match('#(-?(\d+)?)n#', substr($pseudo_class, 11, -1), $a)){
-					if(empty($a[1])){
-						$a=1;
-					}elseif(empty($a[2])){
-						$a=-1;
-					}else{
-						$a=(int)$a[1];
-					}
-				}else{
-					$a=null;
-				}
-				
-				preg_match('#(-?\d+)(?!n)#', $pseudo_class, $b);
-				if(empty($b[1])){
-					$b=0;
-				}else{
-					$b=(int)$b[1];
-				}
-				
-				if($a==1){
-					$xpath.='[position()>'.($b-1).']';
-				}elseif($a==-1){
-					$xpath.='[position()<'.($b-1).']';
-				}elseif($a){
-					$xpath.='[(position()-'.$b.')*'.$a.'>=0 and (position()-'.$b.') mod '.$a.'=0]';
-				}else{
-					$xpath.='[position()='.$b.']';
-				}
-			}
-			
-			if(substr($pseudo_class, 0, 6)==':empty'){
-				$xpath.='[count(./child::node())=0]';
-			}
-			
-			$this->xpath.=$glue.$xpath;
+		preg_match_all('#\#[a-z0-9]+|\.[a-z0-9]+|\[[a-z0-9]+(?:="[a-z0-0]+")?\]|:[a-z-]+(?:\(.*?\))?#', $str, $match);
+		
+		print_r($match);
+		
+		if(empty($match[0])){
+			// nothing ?
 		}else{
-			$this->xpath.=$glue.$xpath;
+			foreach($match[0] as $param){
+				$this->addParam($param);
+			}
 		}
 	}
+	
+	private function addParam($param){
+		echo '<code>'.$param.'</code>';
+		
+		if(substr($param, 0, 4)==':not'){
+			$not=true;
+			$param=substr($param, 5, -1);
+		}else{
+			$not=false;
+		}
+		
+		switch($param{0}){
+			case '#':
+				//po id
+				$this->g_id($param, $not);
+			break;
+			
+			case '.':
+				//klasa
+				$this->g_class($param, $not);
+			break;
+			
+			case '[':
+				//atrybut
+				$this->g_attribute($param, $not);
+			break;
+			
+			case ':':
+				//pseudo-klasa
+				$this->g_pseudoclass($param, $not);
+			break;
+			
+			default:
+				//nic z tych rzeczy, zatem po nazwie + not
+				if($not){
+					$this->xpath.='[name()!="'.$param.'"]';
+				}else{
+					//nigdy nie powinno się zdarzyć
+				}
+			break;
+		}
+	}
+	
+	private function g_id($id, $not=false){
+		$id=substr($id, 1);
+		
+		if(!$not){
+			$this->xpath.='[@id="'.$id.'"]';
+		}else{
+			$this->xpath.='[@id!="'.$id.'"]';
+		}
+	}
+	
+	private function g_class($class, $not=false){
+		$class=substr($class, 1);
+		
+		if(!$not){
+			$this->xpath.='[contains(concat(" ", @class, " "), " '.$class.' ")]';
+		}else{
+			$this->xpath.='[not(contains(concat(" ", @class, " "), " '.$class.' "))]';
+		}
+	}
+	
+	private function g_attribute($attribute, $not){
+		preg_match('#\[([a-z0-9]+)(?:([~^$*|]?)="([a-z0-9]+)"\])?#', $attribute, $match);
+		
+		$attribute=$match[1];
+		$separator=$match[2];
+		$value=$match[3];
+		
+		if($value){
+			switch($separator){
+				case '':
+					$match='@'.$attribute.'="'.$value.'"';
+					break;
+				case '~':
+					$match='contains(concat(" ", @'.$attribute.', " "), " '.$value.' ")';
+					break;
+				case '^':
+					$match='starts-with(@'.$attribute.', '.$value.')';
+					break;
+				case '$':
+					$match='substring(@'.$attribute.', string-length(@'.$attribute.')-'.strlen($value).')="'.$value.'"';
+					break;
+				case '*':
+					$match='contains(@'.$attribute.', "'.$value.'")';
+					break;
+				case '|':
+					$match='contains(@'.$attribute.', " '.$value.'-") or starts-with(@'.$attribute.', "'.$value.'-")';
+					break;
+			}
+		}else{
+			$match='@'.$attribute.'';
+		}
+		
+		if(!$not){
+			$this->xpath.='['.$match.']';
+		}else{
+			$this->xpath.='[not('.$match.')]';
+		}
+	}
+	
+	private function g_pseudoclass($class, $not){
+		preg_match('#^(\:[a-z-]+)(?:\((.*?)\))?$#', $class, $match);
+		
+		//print_r($match);
+		
+		$param=$match[2];
+		$match=$match[1];
+		
+		switch($match){
+			case ':first-child':
+				$match.='../*[position()=1]=.';
+			break;
+			
+			case ':last-child':
+				$match.='../*[position()=last()]=.';
+			break;
+			
+			case ':first-of-type':
+				$match.='position()=1';
+			break;
+			
+			case ':last-of-type':
+				$match.='position()=last()';
+			break;
+			
+			case ':only-of-type':
+				$match.='position()=1 and position()=last()';
+			break;
+			
+			case ':only-child':
+				$match.='../*[position()=1 and position()=last()]=.';
+			break;
+			
+			case ':root':
+				$match.='/=.';
+			break;
+			
+			case ':empty':
+				$match.='count(./child::node())=0';
+			break;
+			
+			case ':lang':
+				$this->g_attribute('[lang|="'.$param.'"]', $not);
+			break;
+			
+			case ':nth-child':
+				/* nth-child start
+				 ^^^^^^^^^^^^^^^^^^^^^^^ sth wrong
+				 */
+				
+				if(preg_match('#(-?(\d+)?)n#', $param, $a)){
+					if(empty($a[1])){
+						$a=1;
+					}elseif(empty($a[2])){
+						$a=-1;
+					}else{
+						$a=(int)$a[1];
+					}
+				}else{
+					$a=null;
+				}
+				
+				preg_match('#(-?\d+)(?!n)#', $param, $b);
+				if(empty($b[1])){
+					$b=0;
+				}else{
+					$b=(int)$b[1];
+				}
+				
+				if($a==1){
+					$match='../*[position()>'.($b-1).']=.';
+				}elseif($a==-1){
+					$match='../*[position()<'.($b-1).']=.';
+				}elseif($a){
+					$match='../*[(position()+'.(-$b).')*'.$a.'>=0 and (position()+'.(-$b).') mod '.$a.'=0]=.';
+				}else{
+					$match='../*[position()='.$b.']=.';
+				}
+				/* nth-child end */
+			break;
+			
+			case ':nth-of-type':
+				/* nth-of-type start */
+				
+				if(preg_match('#(-?(\d+)?)n#', $param, $a)){
+					if(empty($a[1])){
+						$a=1;
+					}elseif(empty($a[2])){
+						$a=-1;
+					}else{
+						$a=(int)$a[1];
+					}
+				}else{
+					$a=null;
+				}
+				
+				preg_match('#(-?\d+)(?!n)#', $param, $b);
+				if(empty($b[1])){
+					$b=0;
+				}else{
+					$b=(int)$b[1];
+				}
+				
+				if($a==1){
+					$match='position()>'.($b-1);
+				}elseif($a==-1){
+					$match='position()<'.($b-1);
+				}elseif($a){
+					$match='(position()+'.(-$b).')*'.$a.'>=0 and (position()+'.(-$b).') mod '.$a.'=0';
+				}else{
+					$match='position()='.$b;
+				}
+				/* nth-of-type end */
+			break;
+		}
+		
+		if(!$not){
+			$this->xpath.='['.$match.']';
+		}else{
+			$this->xpath.='[not('.$match.')]';
+		}
+	}
+		
+		/*
+		:nth-child	
+			
+	}*/
 	
 	private function getobjects(){
 		if(empty($this->xpath)){
@@ -312,9 +379,7 @@ class getNode{
 			$xpath = new DOMXPath($this->xml);
 			$results = $xpath->query($this->xpath, $this->parent);
 			
-			if($this->debug){
-				echo $this->xpath;
-			}
+			echo '<p>Zapytanie to: <code>'.$this->xpath.'</code></p>';
 			
 			return $results;
 		}
@@ -342,12 +407,5 @@ class getNode{
 		return $glue;
 	}
 }
-echo '<br><br><br><br><br><br><br><br><br><br><br><br><pre>';
-
-$str='test#a.b';
-preg_match('#(\#[a-z]+|\.[a-z]+)+#', $str, $match);
-
-print_r($match);
-echo '</pre><br><br><br><br><br><br><br><br><br><br><br><br>';
 
 ?>
